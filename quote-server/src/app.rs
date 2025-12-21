aa64a9fc1eb9e0cbf9f433a462a35dc9356da8db
Roman Zanko <roman.zanko.vl@gmail.com>
2025-12-21 22:30:26 +0400

    Consolidate `StockQuotesGenerator` into `quotes_generator` module to simplify code structure

--- quote-server/src/app.rs
+++ quote-server/src/app.rs
@@ -1,9 +1,7 @@
 mod handler;
 mod quotes_generator;
 mod server_cancellation_token;
-mod stock_quotes_generator;
 
 pub(super) use handler::handle_connection;
-pub(super) use quotes_generator::quotes_generator;
+pub(super) use quotes_generator::{StockQuotesGenerator, quotes_generator};
 pub(super) use server_cancellation_token::ServerCancellationToken;
-pub(super) use stock_quotes_generator::StockQuotesGenerator;
--- quote-server/src/app/quotes_generator.rs
+++ quote-server/src/app/quotes_generator.rs
@@ -1,7 +1,7 @@
-use crate::app::StockQuotesGenerator;
 use crate::app::server_cancellation_token::ServerCancellationToken;
 use crossbeam_channel::Sender;
 use quote_streaming::StockQuote;
+use std::io::{BufRead, BufReader, Read};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
@@ -28,5 +28,50 @@ pub(crate) fn quotes_generator(
             }
         }
         thread::sleep(Duration::from_secs(3));
+    }
+}
+
+pub(crate) struct StockQuotesGenerator {
+    tickers: Vec<String>,
+}
+
+impl StockQuotesGenerator {
+    pub(crate) fn read_from<R: Read>(reader: R) -> Result<Self, std::io::Error> {
+        let buffer = BufReader::new(reader);
+        let mut tickers = Vec::new();
+        for line in buffer.lines() {
+            tickers.push(line?.trim().to_string());
+        }
+        Ok(Self { tickers })
+    }
+
+    pub(super) fn generate(&self) -> Vec<StockQuote> {
+        self.tickers
+            .iter()
+            .map(|ticker| StockQuote::generate(ticker))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_read_from() {
+        let tickers = "AAPL\nMSFT\nGOOGL";
+        let reader = Cursor::new(tickers);
+        let generator = StockQuotesGenerator::read_from(reader).unwrap();
+        assert_eq!(generator.tickers, vec!["AAPL", "MSFT", "GOOGL"]);
+    }
+
+    #[test]
+    fn test_generate() {
+        let generator = StockQuotesGenerator {
+            tickers: vec!["AAPL".to_string(), "MSFT".to_string(), "GOOGL".to_string()],
+        };
+        let quotes = generator.generate();
+        assert_eq!(quotes.len(), 3);
     }
 }
